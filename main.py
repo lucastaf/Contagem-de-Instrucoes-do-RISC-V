64e607c82ffd8328bf87e15e347f5002ef73a58c
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2024-08-24 18:34:37 -0300

    pega informações da instrução

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from riscVInstruction import riscVInstruction
 file = open("ROM", 'r')
 
-
-        
-line = file.readline()
-binario = str(bin(int(line, 16)).zfill(8))
-binario = binario[3:]
-binario = binario.rjust(32,"0")
-binaryObject = riscVInstruction(binario)
-print(binaryObject.get_instruction_type())
-
-#RTYPE  - 011
-#ITYPE - 001
-#STYPE - 010  
-#BTYPE - 110
-#UTYPE - 100
-#JTYPE - 111
-
-#010, 100
+#exit()
+for line in file:
+    binario = str(bin(int(line, 16)).zfill(8))
+    binario = binario[3:]
+    binario = binario.rjust(32,"0")
+    binaryObject = riscVInstruction(binario)
+    print(binaryObject.getInstructionDetails())
--- riscVInstruction.py
+++ riscVInstruction.py
@@ -1,18 +1,30 @@
 class riscVInstruction:
     def __init__(self, binario):
         self.opcode = binario[31-6::]
-        self.rd = binario[31-11:31-6]
-        self.funct3 = binario[31-14: 31-11]
-        self.rs1 = binario[31-19:31-14]
-        self.rs2 = binario[31-24:31-19]
-        self.funct7 = binario[31-31:31-24]
-        self.ITypeImd = self.funct7+self.rs2
-        self.StypeImd = self.funct7+self.rd
-        self.BTypeImd = (binario[0]+binario[31-7]+binario[1:32-25]+binario[31-11:31-7]+ "0")
-        self.UTypeImd = self.funct7 + self.rs2 + self.rs1 + self.funct3 + "00000000000"
-        self.JTypeImd = binario[0] + binario[31-19:31-11] + binario[11] + binario[1:11]
+        self.type = self.getInstructionType()
+        
+        haveRD = ["R","I","U","J"]
+        haveFunct3 = ["R","I","S","B"]
+        haveRS1= ["R","I","S","B"]
+        haveRS2=["R","S","B"]
+        self.rd = binario[31-11:31-6] if type in haveRD else ""
+        self.funct3 = binario[31-14: 31-11] if type in haveFunct3 else ""
+        self.rs1 = binario[31-19:31-14] if type in haveRS1 else ""
+        self.rs2 = binario[31-24:31-19] if type in haveRS2 else ""
+        self.funct7 = binario[31-31:31-24] if type == "R" else "" 
+        
+        imediates = {
+            "I" : self.funct7+self.rs2,
+            "S" : self.funct7+self.rd,
+            "B" : (binario[0]+binario[31-7]+binario[1:32-25]+binario[31-11:31-7]+ "0"),
+            "U" : self.funct7 + self.rs2 + self.rs1 + self.funct3 + "000000000000",
+            "J" : binario[0] + binario[31-19:31-11] + binario[11] + binario[1:11]    
+        }
+        self.imediate = imediates.get(self.type, "")
+        
+        
     
-    def get_instruction_type(self):
+    def getInstructionType(self) -> str:
         opcode = int(self.opcode,2)
         instruction_types = {
             "R": range(0x00, 0x20),
@@ -25,3 +37,61 @@ class riscVInstruction:
         for instruction_type, opcode_range in instruction_types.items():
             if opcode in opcode_range:
                 return instruction_type
+            
+    def getInstructionDetails(self) -> str:
+        details = {
+            "R": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "rd" : self.rd,
+                "funct3" : self.funct3,
+                "rs1": self.rs1,
+                "rs2" : self.rs2,
+                "funct7" : self.funct7
+            }),
+            "I": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "rd" : self.rd,
+                "funct3" : self.funct3,
+                "rs1": self.rs1,
+                "imd" : self.imediate
+            }),
+            "S": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "funct3" : self.funct3,
+                "rs1": self.rs1,
+                "rs2" : self.rs2,
+                "imd" : self.imediate
+            }),
+            "B": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "funct3" : self.funct3,
+                "rs1": self.rs1,
+                "rs2" : self.rs2,
+                "imd" : self.imediate
+            }),
+            "U": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "rd" : self.rd,
+                "imd" : self.imediate
+            }),
+            "J": objectToString({
+                "type" : self.type,
+                "opcode" : self.opcode,
+                "rd" : self.rd,
+                "imd" : self.imediate
+            })
+        }
+        
+        return details.get(self.type)
+    
+def objectToString(object): 
+    returnValue = ""
+    for key, value in object.items():
+        returnValue += key + ": " + value + ","
+    returnValue = returnValue[:-1]
+    return returnValue
