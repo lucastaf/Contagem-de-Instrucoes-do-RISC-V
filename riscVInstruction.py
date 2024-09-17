c6d8f87d3f8b6008a11e6ce3da5924f9d53be50f
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2024-09-17 20:53:27 -0300

    arrumado detecção do type da instrução

--- riscVInstruction.py
+++ riscVInstruction.py
@@ -1,8 +1,9 @@
+from instructionsTypes import intructionsTypes
 class riscVInstruction:
     def __init__(self, binario):
         self.fullInstructions = binario
         self.opcode = binario[31-6::]
-        self.type = self.getInstructionType()
+        self.type = intructionsTypes.get(self.opcode)
         rd = binario[31-11:31-6]
         funct3 = binario[31-14: 31-11]
         rs1 = binario[31-19:31-14]
@@ -28,21 +29,6 @@ class riscVInstruction:
         }
         self.imediate = imediates.get(self.type, "")
         
-        
-    
-    def getInstructionType(self) -> str:
-        opcode = int(self.opcode,2)
-        instruction_types = {
-            "R": range(0x00, 0x20),
-            "I": range(0x20, 0x40),
-            "S": range(0x40, 0x60),
-            "B": range(0x60, 0x80),
-            "U": range(0x80, 0xA0),
-            "J": range(0xA0, 0xC0)
-        }
-        for instruction_type, opcode_range in instruction_types.items():
-            if opcode in opcode_range:
-                return instruction_type
             
     def getInstructionDetails(self) -> str:
         defaultValues = objectToString({
